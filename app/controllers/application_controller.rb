d7b8a0ece4967dec954dd0332b137b7d892a9ced
donrow007 <wn.nw106@gmail.com>
2022-11-16 03:35:00 +0900

    deviseのストロングパラメーターを編集
    deviseの処理を行うコントローラーはGem内にあり編集できないためapplication_controller.rbを編集。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  private
+  # 新規登録時、emailとencrypted_password以外もストロングパラメーターとして許可
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday])
+  end
 end
+
