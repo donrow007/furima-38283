1d44a70e6b97a1a9a726a0dbb6b20dff297e0617
donrow007 <wn.nw106@gmail.com>
2022-11-26 01:01:07 +0900

    商品出品機能　修正5回目

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -47,28 +47,32 @@ describe '商品の出品登録' do
       @item.valid?
       expect(@item.errors.full_messages).to include("Name can't be blank")
     end
-    
     it '商品の説明が空欄だと出品できない' do
       @item.description = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Description can't be blank")
     end
-    it 'カテゴリーの情報が「---」が選択されている場合は出品できない' do
+    it 'カテゴリーの情報が「---」だと出品できない' do
       @item.category_id = 0
       @item.valid?
       expect(@item.errors.full_messages).to include("Category must be other than 0")
     end
-    it '商品の状態の情報が「---」が選択されている場合は出品できない' do
+    it '商品の状態の情報が「---」だと出品できない' do
       @item.item_condition_id = 0
       @item.valid?
       expect(@item.errors.full_messages).to include('Item condition must be other than 0')
     end
-    it '配送料の負担の情報が「---」が選択されている場合は出品できない' do
+    it '配送料の負担の情報が「---」だと出品できない' do
       @item.shipping_cost_id = 0
       @item.valid?
       expect(@item.errors.full_messages).to include('Shipping cost must be other than 0')
     end
-    it '発送までの日数の情報が「---」が選択されている場合は出品できない' do
+    it '発送元の地域の情報が「---」だと出品できない' do
+    @item.prefecture_id = 0
+    @item.valid?
+    expect(@item.errors.full_messages).to include('Prefecture must be other than 0')
+    end
+    it '発送までの日数の情報が「---」だと出品できない' do
       @item.shipping_day_id = 0
       @item.valid?
       expect(@item.errors.full_messages).to include('Shipping day must be other than 0')
