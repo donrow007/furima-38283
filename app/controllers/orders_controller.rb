bfb192364b7559bfb03236b781d553065b8dd44f
donrow007 <wn.nw106@gmail.com>
2022-11-30 19:10:42 +0900

    ordersコントローラーの作成。

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,25 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @order_form = OrderForm.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @order_form = OrderForm.new(order_params)
+    @item = Item.find(params[:item_id])
+    if @order_form.valid?
+      @order_form.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_form).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+end
