ab146410057fbf84e27433decb10420ece6ff1cf
donrow007 <wn.nw106@gmail.com>
2022-12-01 15:13:29 +0900

    Formオブジェクト（order_form）のモデル単体テストの作成。
    テストを通すために、試行錯誤して色々と触る。

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,25 +1,23 @@
 class OrderForm
   include ActiveModel::Model
   
-  attr_accessor :user, :item, :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :order
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :order
 
   with_options presence: true do
     # orderモデルのバリデーション
-    validates :user
-    validates :item
+    validates :user_id
+    validates :item_id
     # destinationモデルのバリデーション
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
     validates :address
-    validates :building_name
-    validates :phone_number, format: { with: /\A[0-9]+{11}\z/, message: 'is invalid' }
-    validates :order
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
   end
 
   def save
-    order = Order.create(user: user, item: item)
+    order = Order.create(user: user_id, item: item_id)
     # ストロングパラメーター
-    Payment.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_numberorder, order: order )
+    Payment.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
